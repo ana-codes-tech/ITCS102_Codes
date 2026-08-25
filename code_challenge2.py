228f33dfae93ba7b716330adc2323b79c657406e
ana-codes-tech <295372750+ana-codes-tech@users.noreply.github.com>
2026-08-25 11:26:58 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,25 @@
+#Create make a python script that operates the following output
+#money = 1540
+#1000 - 1
+#500 - 1
+#
+
+amount = 1520
+a = 1000
+b = 1000 - 500
+c = 1000 - 200
+d = 1000 - 100
+e = 1000 - 50
+f = 1000 - 20
+g = 1000 - 10
+
+
+thousand = 1000//a
+hundred = 500
+tenths = 20
+
+
+print("Number of thousands is", amount//a )
+print("Number of hundreds is", amount//b)
+
+

b7da2fbe25537ebef3a1d0a809ca5de031970179
ana-codes-tech <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-03 10:59:29 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,19 @@
+hobbies = "" #blank string 
+
+hb = input("What are your hobbies ? ")
+
+hobbies += hb + ", "
+
+hb = input("What else ? ")
+
+hobbies += hb + ", "
+
+hb = input("What else ? ")
+
+hobbies += hb + " and "
+
+hb = input("What else ? ")
+
+hobbies += hb
+
+print("My hobbies are ", hobbies)

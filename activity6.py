2af591c3cb44d475ccbee2d73a80922a7175fb99
ana-codes-tech <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-06 19:00:25 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,7 @@
+#First step to dynamic content is thorugh INPUTS (User Input)
+
+Fullname = input("What is your full name? ")
+print("Good day!", Fullname, "Welcome to Vanella Bankya")
+
+Bank_number = int(input("Type your 6 digits Bank number:"))
+print("Bank acount number",Bank_number, "found successfully.") 

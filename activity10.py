e249f17316a033f4b106d4a32e2027d77688928e
Ana Lleva <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-08 10:41:40 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,8 @@
+x = 21
+y = 15
+
+name = "Andoy"
+password = "Mahalkosha"
+
+print( x > y or name == "Not Andoy" and password == "Dkanyamahal")
+print(not( x > y or name == "Not Andoy" and password == "Dkanyamahal"))

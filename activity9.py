19467cc09fe2b0b6e2a62ebe6fb1bc027d503b94
Ana Lleva <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-08 10:24:53 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,28 @@
+#relational operator demo
+
+x = 15
+y = 19
+
+student1 = "Present"
+student2 = "Absent"
+
+#1
+print ( x > y )
+
+#2
+print ( x < y )
+
+#3
+print ( x >= y )
+
+#4
+print ( x <= y )
+
+#5
+print ( x == y )
+
+#6
+print ( student1 == student2 )
+
+#7
+print (student1 != student2)

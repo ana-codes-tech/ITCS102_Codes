c433c002b6b563e2ac0876de0bbe066f77905cac
Ana Lleva <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-08 10:46:17 +0800

    Add address variables and print statements

--- activity10.py
+++ activity10.py
@@ -6,3 +6,8 @@ password = "Mahalkosha"
 
 print( x > y or name == "Not Andoy" and password == "Dkanyamahal")
 print(not( x > y or name == "Not Andoy" and password == "Dkanyamahal"))
+
+address1 = "Sariaya"
+address2 = "Lucena"
+
+print ( x > y and address1 == "Sariaya" )

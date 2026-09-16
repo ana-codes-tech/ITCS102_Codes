4235c0214a30fdbf93bc7096f046156adc05141c
Ana Lleva <295372750+ana-codes-tech@users.noreply.github.com>
2026-09-16 20:41:28 +0800

    Update activity14.py

--- activity14.py
+++ activity14.py
@@ -26,19 +26,17 @@ if age >= 21 and is_employed == True:
             print("Approved at", base_rate)
 
     elif credit_score >= 600 and credit_score < 750:
-        base_rate = 8.0
-
-        if has_collateral == True:
-            base_rate = 7.0
-            print("Approved at base interest rate:", base_rate)
-
-        elif annual_income < 40000:
+        if annual_income <= 40000:
             base_rate = 9.5
             print("Increased risk rate: Approved at", base_rate)
 
+        elif has_collateral == True:
+            base_rate = 7.0
+            print("Approved at base interest rate:", base_rate)
+
         else:
             base_rate = 8.0
-            print("Base rate is", base_rate)
+            print("Base rate with no collateral is", base_rate)
 
     elif credit_score < 600:
         print("Rejected: Credit Score Too Low")
@@ -48,5 +46,3 @@ if age >= 21 and is_employed == True:
 
 else: 
     print("Rejected: Fails baseline criteria")
-
-#test case no.4 na lang not working waaaaaaaaaaaaaaaa
